c03f2fdc98c7d9772cf97b6dae05312bfe7e6398
gage1145 <140093724+gage1145@users.noreply.github.com>
2025-09-17 10:22:48 -0500

    Ok, this should be good now. The package builds.

--- R/call_api.R
+++ R/call_api.R
@@ -1,38 +1,33 @@
-#' Make an API call to the PriLIMS database.
-#' 
-#' Requires an API key and an endpoint URL for the data type being accessed.
-#' 
-#' @param key An API key provided by Priogen.
-#' @param endpoint The endpoint URL. E.g. if accessing the Raw table, then use "raw".
-#' 
-#' @return A data frame object of the API response.
-#' 
+#' Make a call to the PriLIMS API
+#'
+#' Requires an API key provided by Priogen Corp.
+#'
+#' @param key An API key used for accessing PriLIMS API
+#' @param endpoint Endpoint URL; e.g. "raw" if accessing Raw data table.
+#'
 #' @import httr2
 #' @import jsonlite
 #' @import dplyr
-#' 
+#'
 #' @export
-
-API_KEY <- Sys.getenv("PRILIMS_API_KEY")
-BASE_URL <- Sys.getenv("PRILIMS_BASE_URL")
 
 call_api <- function(key, endpoint) {
   url <- paste0("https://priogen.xyz/labdb/api/", endpoint)
   all_results <- list()
-  
+
   repeat {
     req <- request(url) |>
-      req_headers(Authorization = paste("Token", API_KEY))
-    
+      req_headers(Authorization = paste("Token", key))
+
     resp <- req_perform(req)
     data <- resp_body_json(resp, simplifyVector = TRUE)
-    
+
     if (!is.null(data$results)) {
       all_results <- append(all_results, list(data$results))
     } else {
       stop("Response did not contain 'results'")
     }
-    
+
     if (is.null(data$`next`) || data$`next` == "") break
     url <- data$`next`
   }
