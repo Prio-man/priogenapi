72b27c91ddde3047d15067799253ff79b9c8ca29
gage1145 <140093724+gage1145@users.noreply.github.com>
2025-09-18 13:47:23 -0500

    Updated `call_api` to accept query arguments for 'order', 'sample', and 'reaction'.

--- R/call_api.R
+++ R/call_api.R
@@ -3,7 +3,10 @@
 #' Requires an API key provided by Priogen Corp.
 #'
 #' @param key An API key used for accessing PriLIMS API
-#' @param endpoint Endpoint URL; e.g. "raw" if accessing Raw data table.
+#' @param endpoint Endpoint URL; e.g. "results" if accessing Results data table.
+#' @param order Character; the order number
+#' @param sample Character; the sample name
+#' @param reaction Character; the reaction name
 #'
 #' @import httr2
 #' @import jsonlite
@@ -11,15 +14,30 @@
 #'
 #' @export
 
-call_api <- function(key, endpoint) {
-  url <- paste0("https://priogen.xyz/labdb/api/", endpoint)
+call_api <- function(key, endpoint, order=NA, sample=NA, reaction=NA) {
+
+  # Disincentivize user from querying the entire table.
+  if (is.na(order) & is.na(sample) & is.na(reaction)) {
+    cat("\n\n")
+    answer <- "You are about to query the entire %s table. Do you wish to proceed? [Y/n]: " %>%
+      sprintf(endpoint) %>%
+      readline()
+    if (tolower(answer) != "y") stop("API call stopped by user")
+  }
+
+  url <- paste0("http://127.0.0.1:8000/labdb/api/", endpoint)
   all_results <- list()
 
   repeat {
-    req <- request(url) |>
+    req <- request(url) %>%
       req_headers(Authorization = paste("Token", key))
 
-    resp <- req_perform(req)
+    resp <- req %>%
+      {if (!is.na(order)) req_url_query(.data, order=order) else .data} %>%
+      {if (!is.na(sample)) req_url_query(.data, sample=sample) else .data} %>%
+      {if (!is.na(reaction)) req_url_query(.data, reaction=reaction) else .data} %>%
+      req_perform()
+
     data <- resp_body_json(resp, simplifyVector = TRUE)
 
     if (!is.null(data$results)) {
